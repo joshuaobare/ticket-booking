bc631e21cc32e0d585dfdb3e9c5f896ef36f6aa5
joshuaobare <jobare.obare@gmail.com>
2024-02-24 21:58:20 +0300

    update sql files to create database tables

--- ticket-booking.sql
+++ ticket-booking.sql
@@ -1,9 +1,34 @@
+create database ticket_booking;
+use ticket_booking;
+
 CREATE TABLE event (
-    eventID INTEGER AUTO_INCREMENT PRIMARY KEY,
+    event_id INTEGER AUTO_INCREMENT PRIMARY KEY,
     event_name VARCHAR(255) NOT NULL UNIQUE,
     event_location VARCHAR(255),
     vip_ticket_price DECIMAL NOT NULL,
     regular_ticket_price DECIMAL NOT NULL,
     max_attendees INTEGER,
     event_desc VARCHAR(255)
+);
+
+CREATE TABLE user (
+    user_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(20) NOT NULL,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(50) NOT NULL,
+    is_admin BOOLEAN
+);
+
+CREATE TABLE ticketing (
+    ticket_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    event_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP (),
+    ticket_price DECIMAL NOT NULL,
+    ticket_type VARCHAR(50) NOT NULL,
+    FOREIGN KEY (event_id)
+        REFERENCES event (event_id),
+    FOREIGN KEY (user_id)
+        REFERENCES user (user_id)
 );
