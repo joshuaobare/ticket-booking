e3179d7b2595893b5e71c95cf6e4a34d460b5b65
joshuaobare <jobare.obare@gmail.com>
2024-02-24 23:29:13 +0300

    create event form and formData state within App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,91 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [formData, setFormData] = useState({
+    event_name: "",
+    event_location: "",
+    vip_ticket_price: "",
+    regular_ticket_price: "",
+    max_attendees: "",
+    event_desc: "",
+    date: "",
+  });
+  const formSubmit = () => {};
 
+  const formChange = (e) => {};
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form action="" onSubmit={formSubmit}>
+        <div>
+          <label htmlFor="event_name">Name</label>
+          <input
+            type="text"
+            name="event_name"
+            onChange={formChange}
+            value={formData.event_name}
+          />
+        </div>
+        <div>
+          <label htmlFor="event_location">Location</label>
+          <input
+            type="text"
+            name="event_location"
+            onChange={formChange}
+            value={formData.event_location}
+          />
+        </div>
+        <div>
+          <label htmlFor="vip_ticket_price">Vip Ticket Price</label>
+          <input
+            type="text"
+            name="vip_ticket_price"
+            onChange={formChange}
+            value={formData.vip_ticket_price}
+          />
+        </div>
+        <div>
+          <label htmlFor="regular_ticket_price">Regular Ticket Price</label>
+          <input
+            type="text"
+            name="regular_ticket_price"
+            onChange={formChange}
+            value={formData.regular_ticket_price}
+          />
+        </div>
+        <div>
+          <label htmlFor="max_attendees">Max Attendees</label>
+          <input
+            type="text"
+            name="max_attendees"
+            onChange={formChange}
+            value={formData.max_attendees}
+          />
+        </div>
+        <div>
+          <label htmlFor="event_desc">Event Description</label>
+          <input
+            type="text"
+            name="event_desc"
+            onChange={formChange}
+            value={formData.event_desc}
+          />
+        </div>
+        <div>
+          <label htmlFor="date">Date and Time</label>
+          <input
+            type="datetime-local"
+            name="date"
+            onChange={formChange}
+            value={formData.date}
+          />
+        </div>
+        <button>Submit</button>
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
