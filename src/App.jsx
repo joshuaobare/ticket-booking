1ae177ad44e6ced92d021420df86cad5c474dd4d
joshuaobare <jobare.obare@gmail.com>
2024-02-25 20:53:15 +0300

    add routes to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,32 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Login from "./components/Login"
+import Login from "./components/Login";
 import UserHome from "./components/UserHome";
-import CreateEvent from "./components/CreateEvent"
+import CreateEvent from "./components/CreateEvent";
+import FullEvent from "./components/FullEvent";
+import AdminHome from "./components/AdminHome";
+import { HashRouter, BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [loggedIn, setLoggedIn] = useState(false)
-  const [isAdmin, setIsAdmin] = useState(false)
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [isAdmin, setIsAdmin] = useState(false);
   return (
-  <div>
-    {/* <Login setLoggedIn={setLoggedIn} setIsAdmin={setIsAdmin}/> */}
-    <UserHome />
-    </div>);
+    <div className="App">
+      {/* <Login setLoggedIn={setLoggedIn} setIsAdmin={setIsAdmin}/> */}
+
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route
+            path="/"
+            exact
+            element={isAdmin ? <AdminHome /> : <UserHome />}
+          />
+          <Route path="/event/:id" exact element={<FullEvent />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/FullEvent.jsx
@@ -0,0 +1,5 @@
+const FullEvent = () => {
+
+}
+
+export default FullEvent
