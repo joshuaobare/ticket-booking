6269be3826b22fac7532c2c99e0700caa0f1093c
joshuaobare <jobare.obare@gmail.com>
2024-02-25 20:01:32 +0300

    create dateHandler function in EventCard component

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,21 +1,24 @@
 import poster from "../assets/randomposter.webp";
-import {format} from "date-fns"
+import { format } from "date-fns";
 const EventCard = ({ event }) => {
   const style = {
     backgroundImage: `url(${poster})`,
   };
 
   const dateHandler = (date) => {
-    formattedDate = new Date(date)
-  }
-  
+    const dateObj = new Date(date);
+    return `${format(dateObj, "EEE do MMMM")} at ${format(dateObj, "h:m aaa")}`
+  };
+
   return (
     <div key={event.event_id} className="event-card">
       <div className="event-card-poster" style={style}></div>
-      <div className="event-card-date">{event.date}</div>
-      <div className="event-card-name">{event.event_name}</div>      
+      <div className="event-card-date">{dateHandler(event.date)}</div>
+      <div className="event-card-name">{event.event_name}</div>
       <div className="event-card-date">{event.event_location}</div>
-      <div>KShs. {event.regular_ticket_price} - KShs.{event.vip_ticket_price}</div>
+      <div>
+        KShs. {event.regular_ticket_price} - KShs.{event.vip_ticket_price}
+      </div>
       <button>Book</button>
     </div>
   );
