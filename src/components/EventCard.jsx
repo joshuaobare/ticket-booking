2dd1786bf24156550b2d9255d79bfa4ffba845c8
joshuaobare <jobare.obare@gmail.com>
2024-02-25 19:51:34 +0300

    style EventCard component

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,12 +1,19 @@
-const EventCard = ({event}) => {
-    return (
-        <div key={event.event_id}>
-            <div>{event.event_name}</div>
-            <div>{event.date}</div>
-            <div>{event.event_location}</div>
-            <button>Book</button>
-        </div>
-    )
+import poster from "../assets/randomposter.webp";
+const EventCard = ({ event }) => {
+  const style = {
+    backgroundImage: `url(${poster})`,
+  };
+  
+  return (
+    <div key={event.event_id} className="event-card">
+      <div className="event-card-poster" style={style}></div>
+      <div className="event-card-date">{event.date}</div>
+      <div className="event-card-name">{event.event_name}</div>      
+      <div className="event-card-date">{event.event_location}</div>
+      <div>KShs. {event.regular_ticket_price} - KShs.{event.vip_ticket_price}</div>
+      <button>Book</button>
+    </div>
+  );
 };
 
 export default EventCard;
