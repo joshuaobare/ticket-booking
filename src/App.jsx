ad097a289570a2e74e64ce4eded992463b7046a2
joshuaobare <jobare.obare@gmail.com>
2024-02-25 16:09:54 +0300

    add onChange and onSubmit handler functions to Login component and add loginuser php file

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import SignUp from "./components/SignUp"
+import Login from "./components/Login"
 
 function App() {
-  return <SignUp />;
+  return <Login />;
 }
 
 export default App;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,17 +1,51 @@
 import "../App.css";
 const Login = () => {
+    const [formData, setFormData] = useState({        
+        email: "",
+        password: "",
+      });
+      const formSubmit = async (e) => {
+        e.preventDefault();
+        try {
+          const request = await fetch(
+            "http://localhost:8080/ticket-booking/php/loginuser.php",
+            {
+              method: "POST",
+              headers: { "Content-type": "application/json" },
+              body: JSON.stringify(formData),
+            }
+          );
+          const response = await request.json();
+          console.log(response);
+          setFormData({            
+            email: "",
+            password: ""
+          });
+        } catch (err) {
+          console.log(err);
+        }
+      };
+    
+      const formChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevState) => {
+          return { ...prevState, [name]: value };
+        });
+      };
   return (
     <div>
       <form action="" method="post">
         <div>
           <label htmlFor="">Email</label>
-          <input type="email" />
+          <input type="email" name="email" id="email" value={formData.email}/>
         </div>
         <div>
           <label htmlFor="">Password</label>
-          <input type="password" />
+          <input type="password" name="password" id="password" value={formData.password}/>
         </div>
       </form>
     </div>
   );
 };
+
+export default Login;
