0937d3dcc73ad24e2ade9c21d9e3c0c3208f88a5
joshuaobare <jobare.obare@gmail.com>
2024-02-25 15:48:27 +0300

    update signUp component with onchange and formsubit handler functions

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,24 +1,86 @@
 const SignUp = () => {
+  const [formData, setFormData] = useState({
+    first_name: "",
+    last_name: "",
+    email: "",
+    password: "",
+  });
+  const formSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const request = await fetch(
+        "http://localhost:8080/ticket-booking/php/createuser.php",
+        {
+          method: "POST",
+          headers: { "Content-type": "application/json" },
+          body: JSON.stringify(formData),
+        }
+      );
+      const response = await request.json();
+      console.log(response);
+      setFormData({
+        first_name: "",
+        last_name: "",
+        email: "",
+        password: "",
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const formChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => {
+      return { ...prevState, [name]: value };
+    });
+  };
+
   return (
     <div>
-      <form action=""></form>
-      <div>
-        <label htmlFor="first_name">First Name</label>
-        <input type="text" id="first_name" name="first_name"/>
-      </div>
-      <div>
-        <label htmlFor="last_name">Last Name</label>
-        <input type="text" name="last_name" id="last_name"/>
-      </div>
-      <div>
-        <label htmlFor="email">Email</label>
-        <input type="email" name="email" id="email"/>
-      </div>
-      <div>
-        <label htmlFor="password">Password</label>
-        <input type="password" name="password" id="password"/>
-      </div>
-      <button>Submit</button>
+      <form action="" method="post" onSubmit={formSubmit}>
+        <div>
+          <label htmlFor="first_name">First Name</label>
+          <input
+            type="text"
+            id="first_name"
+            name="first_name"
+            onChange={formChange}
+            value={formData.first_name}
+          />
+        </div>
+        <div>
+          <label htmlFor="last_name">Last Name</label>
+          <input
+            type="text"
+            name="last_name"
+            id="last_name"
+            onChange={formChange}
+            value={formData.last_name}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            onChange={formChange}
+            value={formData.email}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            onChange={formChange}
+            value={formData.password}
+          />
+        </div>
+        <button>Submit</button>
+      </form>
     </div>
   );
 };
