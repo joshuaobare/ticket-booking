0791bcc127d6878446d621d29da5bf297eaaa461
joshuaobare <jobare.obare@gmail.com>
2024-02-25 16:27:11 +0300

    add logic into loginuser php file

---
+++ php/loginuser.php
@@ -0,0 +1,34 @@
+<?php
+include_once("./pdo.php");
+include_once("./header.php");
+
+$data = file_get_contents('php://input');
+$_POST = json_decode($data, true);
+
+if (
+    isset($_POST["email"]) && isset($_POST["password"])){
+        try {
+            $sql = "SELECT * FROM USER WHERE EMAIL = :EMAIL";
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute(
+                array(                    
+                    ":email" => $_POST["email"]
+                )
+            );
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            if ($row){
+                $password_verification = password_verify($_POST['password'], $row['password']);
+
+                if ($password_verification) {
+                    $res = array("user" => $row, "message" => "You are now logged in!");
+                    echo json_encode($res);
+                } else {
+                    $res = array("error" => "Unsuccessful login");
+                }
+            }
+            
+        } catch (Exception $e) {
+            echo "An error has occurred";
+        }
+}
