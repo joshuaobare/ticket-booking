42b0b2a9f4d7019c56f9c7db93a723164c59cec2
joshuaobare <jobare.obare@gmail.com>
2024-02-26 22:54:10 +0300

    add an insertBooking function in bookticket.php to reduce redundancy

--- php/bookticket.php
+++ php/bookticket.php
@@ -5,8 +5,23 @@ include_once("./header.php");
 $data = file_get_contents('php://input');
 $_POST = json_decode($data, true);
 
-function insertBooking() {
-    
+function insertBooking($pdo, $event_id, $user_id, $ticket_price, $ticket_type) {  
+    try {
+       $sql = "INSERT INTO TICKETING (EVENT_ID, USER_ID, TICKET_PRICE, TICKET_TYPE) VALUES (:EVENT_ID, :USER_ID, :TICKET_PRICE, :TICKET_TYPE)";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute((
+        array(
+            ":EVENT_ID" =>$event_id, 
+            ":USER_ID" =>$user_id, 
+            ":TICKET_PRICE" =>$ticket_price, 
+            ":TICKET_TYPE" =>$ticket_type
+        ) 
+        ));
+    $data = array("res" => "Booking made successfully");        
+    echo json_encode($data);
+    } catch (Exception $e) {
+        echo json_encode(array("error" => $e));
+    }
 
 }
 
