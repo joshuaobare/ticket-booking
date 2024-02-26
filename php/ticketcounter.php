e9abe72afd1c972efd839ce2f2832bfb0851ef37
joshuaobare <jobare.obare@gmail.com>
2024-02-26 15:58:20 +0300

    added counter for both vip and regular tickets in the ticketcounter file

--- php/ticketcounter.php
+++ php/ticketcounter.php
@@ -7,21 +7,37 @@ $reg_count = 0;
 
 if (isset($_GET['event_id']) && isset($_GET['user_id'])) {
     try {
-        $sql = "SELECT TICKET_TYPE, COUNT(*) AS 'TICKETS PURCHASED' FROM TICKETING WHERE EVENT_ID = :EVENT_ID AND USER_ID = :USER_ID GROUP BY TICKET_TYPE";
+        $sql = "SELECT COUNT(*) AS 'TICKETS PURCHASED' FROM TICKETING WHERE EVENT_ID = :EVENT_ID AND USER_ID = :USER_ID AND TICKET_TYPE= 'VIP'";
         $stmt = $pdo->prepare($sql);
         $stmt->execute((
             array(
                 ":EVENT_ID" => $_GET['event_id'],
                 ":USER_ID" => $_GET['user_id']
             )
-        ));
-        echo "start\n";
+        ));              
+        
         while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
             foreach ($row as $key => $value) {
-                echo "$key + $value\n";
+                $vip_count = $value;
             }
         }
-        echo $row;
+
+        $sql = "SELECT COUNT(*) AS 'TICKETS PURCHASED' FROM TICKETING WHERE EVENT_ID = :EVENT_ID AND USER_ID = :USER_ID AND TICKET_TYPE= 'REGULAR'";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute((
+            array(
+                ":EVENT_ID" => $_GET['event_id'],
+                ":USER_ID" => $_GET['user_id']
+            )
+        ));              
+        
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            foreach ($row as $key => $value) {
+                $reg_count = $value;
+            }
+        }
+        $res = array("vip_count"=> $vip_count, "reg_count"=> $reg_count);
+        echo json_encode($res);
 
     } catch (Exception $e) {
         echo $e;
