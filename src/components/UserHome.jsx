f77eefb910e8ceba0f4ffd2bff6414e2a2540139
joshuaobare <jobare.obare@gmail.com>
2024-02-27 22:27:15 +0300

    add userData state to Home component and accompanying setters as well as a conditionally rendered create event button

--- src/components/UserHome.jsx
+++ src/components/UserHome.jsx
@@ -5,32 +5,80 @@ import "../styles/UserHome.css";
 
 const UserHome = () => {
   const [allEvents, setAllEvents] = useState([]);
+  const [userData, setUserData] = useState({
+    user_id: null,
+    first_name: "",
+    last_name: "",
+    email: "",
+    is_admin: false,
+  });
+
+  const userVerification = async (user_id) => {
+    try {
+      const request = await fetch(
+        `http://localhost:8080/ticket-booking/php/fetchuser.php?id=${user_id}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-type": "application/json",
+          },
+        }
+      );
+      const response = await request.json();
+
+      if (response.user) {
+        setUserData({
+          user_id: response.user.user_id,
+          first_name: response.user.first_name,
+          last_name: response.user.last_name,
+          email: response.user.email,
+          is_admin: response.user.is_admin === "1" ? true : false,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const fetchEvents = async () => {
+    try {
+      const request = await fetch(
+        "http://localhost:8080/ticket-booking/php/fetchevents.php",
+        {
+          method: "GET",
+          headers: {
+            "Content-type": "application/json",
+          },
+        }
+      );
+      const response = await request.json();
+      setAllEvents(response.events);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
-    const fetchEvents = async () => {
-      try {
-        const request = await fetch(
-          "http://localhost:8080/ticket-booking/php/fetchevents.php",
-          {
-            method: "GET",
-            headers: {
-              "Content-type": "application/json",
-            },
-          }
-        );
-        const response = await request.json();
-        setAllEvents(response.events);
-      } catch (error) {
-        console.log(error);
-      }
-    };
     fetchEvents();
+    if (localStorage.getItem("user_id")) {
+      userVerification(localStorage.getItem("user_id"));
+    }
   }, []);
 
   return (
     <div className="UserHome">
       <div className="UserHome-cont">
         <h1 className="UserHome-heading">All Events</h1>
+        {userData.is_admin ? (
+          <div className="UserHome-create-event-cont">
+            <button className="UserHome-create-event-btn">
+              <span className="material-symbols-outlined">calendar_add_on</span>
+              Create Event
+            </button>
+          </div>
+        ) : (
+          ""
+        )}
         <div className="UserHome-event-count">
           {allEvents.length} Event{allEvents.length === 1 ? "" : "s"}
         </div>
