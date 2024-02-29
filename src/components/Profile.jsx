1ff15200c41f912d9055a7c97dbf44854100a079
joshuaobare <jobare.obare@gmail.com>
2024-02-29 11:49:10 +0300

    add fetchUser function in Profile component

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import "../styles/Profile.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
 const Profile = () => {
   const { id } = useParams();
   const [userData, setUserData] = useState({
@@ -10,6 +11,38 @@ const Profile = () => {
     email: "",
     is_admin: false,
   });
+
+  const fetchUser = async (user_id) => {
+    try {
+      const request = await fetch(
+        `http://localhost:8080/ticket-booking/php/fetchuser.php?id=${user_id}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-type": "application/json",
+          },
+        }
+      );
+      const response = await request.json();
+
+      if (response.user) {
+        setUserData({
+          user_id: response.user.user_id,
+          first_name: response.user.first_name,
+          last_name: response.user.last_name,
+          email: response.user.email,
+          is_admin: response.user.is_admin === "1" ? true : false,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser(id);
+  }, []);
+
   return (
     <div className="Profile">
       <main className="profile-main">
