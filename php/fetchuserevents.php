e0906c8a003257e4f783940e321d44ed1b250538
joshuaobare <jobare.obare@gmail.com>
2024-02-29 12:43:26 +0300

    edit sql statement on inner join in fetchuserevents php file to remove unnecessary fields

--- php/fetchuserevents.php
+++ php/fetchuserevents.php
@@ -3,16 +3,20 @@
 include_once("./pdo.php");
 include_once("./header.php");
 
-$res = array("message"=> array()); 
+$res = array("message" => array());
 if (isset($_GET['user_id'])) {
-    try {        
-        $sql = "SELECT * FROM TICKETING INNER JOIN EVENT ON TICKETING.EVENT_ID = EVENT.EVENT_ID WHERE USER_ID = :USER_ID";
+    try {
+        $sql = "SELECT EVENT.DATE, EVENT.EVENT_DESC, EVENT.EVENT_LOCATION, EVENT.EVENT_NAME, EVENT.IMAGE,TICKETING.TICKET_PRICE, TICKETING.TICKET_TYPE, TICKETING.TIMESTAMP 
+        FROM TICKETING 
+        INNER JOIN EVENT 
+        ON TICKETING.EVENT_ID = EVENT.EVENT_ID 
+        WHERE USER_ID = :USER_ID";
         $stmt = $pdo->prepare($sql);
         $stmt->execute(
             array(
                 ":USER_ID" => $_GET['user_id'],
             )
-            );
+        );
 
         global $res;
         while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
@@ -22,6 +26,6 @@ if (isset($_GET['user_id'])) {
     } catch (Exception $e) {
         echo json_encode(array("error" => $e));
     }
-    
+
 
 }
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -11,6 +11,22 @@ const Profile = () => {
     email: "",
     is_admin: false,
   });
+  const [eventData, setEventData] = useState({
+    "ticket_id": "1",
+    "event_id": "3",
+    "user_id": "2",
+    "timestamp": "2024-02-26 14:58:52",
+    "ticket_price": "50000",
+    "ticket_type": "vip",
+    "event_name": "Rave",
+    "event_location": "Kisumu",
+    "vip_ticket_price": "50000",
+    "regular_ticket_price": "20000",
+    "max_attendees": "20",
+    "event_desc": "Come Party With Us!",
+    "date": "2024-02-11 18:17:00",
+    "image": "/src/assets/blackbg.jpg"
+})
 
   const fetchUser = async (user_id) => {
     try {
@@ -39,9 +55,31 @@ const Profile = () => {
     }
   };
 
+  const fetchEvents = async () => {
+    try {
+      const request = await fetch(
+        `http://localhost:8080/ticket-booking/php/fetchuserevents.php?user_id=${userData.user_id}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-type": "application/json",
+          },
+        }
+      );
+      const response = await request.json()
+      console.log(response)
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
     fetchUser(id);
   }, []);
+
+  useEffect(() => {
+    fetchEvents();
+  }, [userData]);
 
   return (
     <div className="Profile">
@@ -58,7 +96,7 @@ const Profile = () => {
           </div>
         </section>
         <section className="profile-orders">
-            <h1>Orders</h1>
+          <h1>Orders</h1>
         </section>
       </main>
     </div>
