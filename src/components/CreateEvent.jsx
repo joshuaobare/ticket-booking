2ccf93d3c68085bf76fdf12dc58f2f5c48a0d127
joshuaobare <jobare.obare@gmail.com>
2024-02-29 21:31:26 +0300

    add media query to UserHome component css

--- src/components/CreateEvent.jsx
+++ src/components/CreateEvent.jsx
@@ -12,9 +12,9 @@ function CreateEvent({ dialogOpen, dialogToggler, fetchEvents }) {
     regular_ticket_price: "",
     max_attendees: "",
     event_desc: "",
-    date: "",    
+    date: "",
   });
-  const [image, setImage] = useState("")
+  const [image, setImage] = useState("");
 
   function convertToBase64(file) {
     return new Promise((resolve, reject) => {
@@ -30,7 +30,7 @@ function CreateEvent({ dialogOpen, dialogToggler, fetchEvents }) {
   }
   const handleImage = async (e) => {
     setImage(await convertToBase64(e.target.files[0]));
-  }
+  };
 
   const formSubmit = async (e) => {
     e.preventDefault();
@@ -41,11 +41,11 @@ function CreateEvent({ dialogOpen, dialogToggler, fetchEvents }) {
         {
           method: "POST",
           headers: { "Content-type": "application/json" },
-          body: JSON.stringify({...formData, image}),
+          body: JSON.stringify({ ...formData, image }),
         }
       );
       const response = await request.json();
-      
+
       if (response.message) {
         setFormData({
           event_name: "",
@@ -54,17 +54,16 @@ function CreateEvent({ dialogOpen, dialogToggler, fetchEvents }) {
           regular_ticket_price: "",
           max_attendees: "",
           event_desc: "",
-          date: "",        
+          date: "",
         });
-        fetchEvents()
-        dialogToggler()
+        fetchEvents();
+        dialogToggler();
       }
-      
     } catch (err) {
       console.log(err);
     }
   };
-  
+
   const formChange = (e) => {
     const { name, value, type, files } = e.target;
 
@@ -82,122 +81,130 @@ function CreateEvent({ dialogOpen, dialogToggler, fetchEvents }) {
   };
   return (
     <Dialog open={dialogOpen}>
-      <div className="create-event-header">
-        <h2 className="create-event-heading">Create Event</h2>
-        <button className="create-event-close" onClick={dialogToggler}>
-          <Close />
-        </button>
-      </div>
+      <div>
+        <div className="create-event-header">
+          <h2 className="create-event-heading">Create Event</h2>
+          <button className="create-event-close" onClick={dialogToggler}>
+            <Close />
+          </button>
+        </div>
 
-      <form
-        action=""
-        method="post"
-        onSubmit={formSubmit}
-        className="create-event-form"
-      >
-        <div className="create-event-form-item">
-          <label htmlFor="event_name" className="create-event-form-item-label">
-            Name
-          </label>
-          <input
-            type="text"
-            name="event_name"
-            onChange={formChange}
-            value={formData.event_name}
-          />
-        </div>
-        <div className="create-event-form-item">
-          <label
-            htmlFor="event_location"
-            className="create-event-form-item-label"
-          >
-            Location
-          </label>
-          <input
-            type="text"
-            name="event_location"
-            onChange={formChange}
-            value={formData.event_location}
-          />
-        </div>
-        <div className="create-event-form-item">
-          <label
-            htmlFor="vip_ticket_price"
-            className="create-event-form-item-label"
-          >
-            Vip Ticket Price
-          </label>
-          <input
-            type="text"
-            name="vip_ticket_price"
-            onChange={formChange}
-            value={formData.vip_ticket_price}
-          />
-        </div>
-        <div className="create-event-form-item">
-          <label
-            htmlFor="regular_ticket_price"
-            className="create-event-form-item-label"
-          >
-            Regular Ticket Price
-          </label>
-          <input
-            type="text"
-            name="regular_ticket_price"
-            onChange={formChange}
-            value={formData.regular_ticket_price}
-          />
-        </div>
-        <div className="create-event-form-item">
-          <label
-            htmlFor="max_attendees"
-            className="create-event-form-item-label"
-          >
-            Max Attendees
-          </label>
-          <input
-            type="text"
-            name="max_attendees"
-            onChange={formChange}
-            value={formData.max_attendees}
-          />
-        </div>
-        <div className="create-event-form-item">
-          <label htmlFor="event_desc" className="create-event-form-item-label">
-            Event Description
-          </label>
-          <textarea
-            type="text"
-            name="event_desc"
-            onChange={formChange}
-            value={formData.event_desc}
-            id="event_desc"
-          />
-        </div>
-        <div className="create-event-form-item">
-          <label htmlFor="image" className="create-event-form-item-label">
-            Poster
-          </label>
-          <input
-            type="file"
-            name="image"
-            accept=".jpg, .png, .jpeg, .gif"
-            onChange={handleImage}
-          />
-        </div>
-        <div className="create-event-form-item">
-          <label htmlFor="date" className="create-event-form-item-label">
-            Date and Time
-          </label>
-          <input
-            type="datetime-local"
-            name="date"
-            onChange={formChange}
-            value={formData.date}
-          />
-        </div>
-        <button className="create-event-form-submit-btn">Submit</button>
-      </form>
+        <form
+          action=""
+          method="post"
+          onSubmit={formSubmit}
+          className="create-event-form"
+        >
+          <div className="create-event-form-item">
+            <label
+              htmlFor="event_name"
+              className="create-event-form-item-label"
+            >
+              Name
+            </label>
+            <input
+              type="text"
+              name="event_name"
+              onChange={formChange}
+              value={formData.event_name}
+            />
+          </div>
+          <div className="create-event-form-item">
+            <label
+              htmlFor="event_location"
+              className="create-event-form-item-label"
+            >
+              Location
+            </label>
+            <input
+              type="text"
+              name="event_location"
+              onChange={formChange}
+              value={formData.event_location}
+            />
+          </div>
+          <div className="create-event-form-item">
+            <label
+              htmlFor="vip_ticket_price"
+              className="create-event-form-item-label"
+            >
+              Vip Ticket Price
+            </label>
+            <input
+              type="text"
+              name="vip_ticket_price"
+              onChange={formChange}
+              value={formData.vip_ticket_price}
+            />
+          </div>
+          <div className="create-event-form-item">
+            <label
+              htmlFor="regular_ticket_price"
+              className="create-event-form-item-label"
+            >
+              Regular Ticket Price
+            </label>
+            <input
+              type="text"
+              name="regular_ticket_price"
+              onChange={formChange}
+              value={formData.regular_ticket_price}
+            />
+          </div>
+          <div className="create-event-form-item">
+            <label
+              htmlFor="max_attendees"
+              className="create-event-form-item-label"
+            >
+              Max Attendees
+            </label>
+            <input
+              type="text"
+              name="max_attendees"
+              onChange={formChange}
+              value={formData.max_attendees}
+            />
+          </div>
+          <div className="create-event-form-item">
+            <label
+              htmlFor="event_desc"
+              className="create-event-form-item-label"
+            >
+              Event Description
+            </label>
+            <textarea
+              type="text"
+              name="event_desc"
+              onChange={formChange}
+              value={formData.event_desc}
+              id="event_desc"
+            />
+          </div>
+          <div className="create-event-form-item">
+            <label htmlFor="image" className="create-event-form-item-label">
+              Poster
+            </label>
+            <input
+              type="file"
+              name="image"
+              accept=".jpg, .png, .jpeg, .gif"
+              onChange={handleImage}
+            />
+          </div>
+          <div className="create-event-form-item">
+            <label htmlFor="date" className="create-event-form-item-label">
+              Date and Time
+            </label>
+            <input
+              type="datetime-local"
+              name="date"
+              onChange={formChange}
+              value={formData.date}
+            />
+          </div>
+          <button className="create-event-form-submit-btn">Submit</button>
+        </form>
+      </div>
     </Dialog>
   );
 }
