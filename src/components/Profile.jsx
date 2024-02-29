db07464d9548d32279cd075907cf4f73281a74e3
joshuaobare <jobare.obare@gmail.com>
2024-02-29 11:41:31 +0300

    create Profile component layout

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,5 +1,17 @@
+import "../styles/Profile.css"
 const Profile = () => {
-
+    return(
+        <div className="Profile">
+            <main className="profile-main">
+                <div>
+                    User Details
+                </div>
+                <div>
+                    Orders
+                </div>
+            </main>
+        </div>
+    )
 }
 
 export default Profile;
