dcb26ec6b269aae506c55acfaa4e567551714133
joshuaobare <jobare.obare@gmail.com>
2024-02-29 12:33:41 +0300

    add fetchuserevents php files to be called in the Profile component

---
+++ php/fetchuserevents.php
@@ -0,0 +1,27 @@
+<?php
+
+include_once("./pdo.php");
+include_once("./header.php");
+
+$res = array("message"=> array()); 
+if (isset($_GET['user_id'])) {
+    try {        
+        $sql = "SELECT * FROM TICKETING INNER JOIN EVENT ON TICKETING.EVENT_ID = EVENT.EVENT_ID WHERE USER_ID = :USER_ID";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute(
+            array(
+                ":USER_ID" => $_GET['user_id'],
+            )
+            );
+
+        global $res;
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            array_push($res["message"], $row);
+        }
+        echo json_encode($res);
+    } catch (Exception $e) {
+        echo json_encode(array("error" => $e));
+    }
+    
+
+}
