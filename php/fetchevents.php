ada4d9185494a9a92387c1579f4ee60df7cfef1d
joshuaobare <jobare.obare@gmail.com>
2024-03-01 09:07:06 +0300

    add check to fetch events with currdate greater than or equal to today

--- php/fetchevents.php
+++ php/fetchevents.php
@@ -6,14 +6,14 @@ $data = file_get_contents('php://input');
 $_POST = json_decode($data, true);
 
 // this file fetches all events in the database
-try{
-    $sql = "SELECT * FROM EVENT";
+try {
+    $sql = "SELECT * FROM EVENT WHERE DATE>=CURDATE()";
     $stmt = $pdo->query($sql);
     $data = array();
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
         array_push($data, $row);
     }
-    $res = array("events"=> $data, "message" => "Successfully fetched all events");
+    $res = array("events" => $data, "message" => "Successfully fetched all events");
     echo json_encode($res);
 } catch (Exception $e) {
     echo "An error has occurred";
